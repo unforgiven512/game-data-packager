e9e0b85f799a71c36277640608a921793d8cdb5d
Jon Dowland <jon@alcopop.org>
2008-07-18 00:35:41 +0000

    directives in correct order

--- doom-common.mk
+++ doom-common.mk
@@ -3,22 +3,20 @@
 
 $(IWAD)DEB=$(IWAD)-wad_$(VERSION)_all.deb
 
-$(IWAD)TARGETS := $(IWAD)-wad/DEBIAN/md5sums $(IWAD)-wad/DEBIAN/control $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/changelog.gz $(IWAD)-wad/usr/share/pixmaps/$(IWAD).xpm $(IWAD)-wad/DEBIAN/postinst $(IWAD)-wad/DEBIAN/prerm $(IWAD)-wad/usr/share/applications/$(IWAD)-wad.desktop $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/README.Debian $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/copyright $(IWAD)DIRS
+$(IWAD)TARGETS := $(IWAD)DIRS $(IWAD)-wad/DEBIAN/control $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/changelog.gz $(IWAD)-wad/usr/share/pixmaps/$(IWAD).xpm $(IWAD)-wad/DEBIAN/postinst $(IWAD)-wad/DEBIAN/prerm $(IWAD)-wad/usr/share/applications/$(IWAD)-wad.desktop $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/README.Debian $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/copyright $(IWAD)-wad/DEBIAN/md5sums 
 
-DIRS := $(IWAD)-wad
+DIRS := \
 	$(IWAD)-wad/DEBIAN \
-	$(IWAD)-wad/usr \
-	$(IWAD)-wad/usr/share \
 	$(IWAD)-wad/usr/share/pixmaps \
 	$(IWAD)-wad/usr/share/applications \
-	$(IWAD)-wad/usr/share/doc \
 	$(IWAD)-wad/usr/share/doc/$(IWAD)-wad \
+	$(IWAD)-wad/usr/share/doc \
+	$(IWAD)-wad/usr/share/games/game-data-packager \
+	$(IWAD)-wad/usr/share/games/doom \
 	$(IWAD)-wad/usr/share/games \
-	$(IWAD)-wad/usr/share/games/doom
-	$(IWAD)-wad/usr/share/games/game-data-packager
-
-$(IWAD)DIRS:
-	mkdir -p $(DIRS)
+	$(IWAD)-wad/usr/share \
+	$(IWAD)-wad/usr \
+	$(IWAD)-wad
 
 $($(IWAD)DEB): $($(IWAD)TARGETS) fixperms 
 	if [ `id -u` -eq 0 ]; then \
@@ -26,6 +24,9 @@ $($(IWAD)DEB): $($(IWAD)TARGETS) fixperms
 	else \
 		fakeroot dpkg-deb -b $(IWAD)-wad $@; \
 	fi
+
+$(IWAD)DIRS:
+	mkdir -p $(DIRS)
 
 $(IWAD)-wad/usr/share/doc/$(IWAD)-wad/copyright:
 	m4 -DPACKAGE=$(IWAD)-wad -DIWAD=$(IWAD).wad \
@@ -72,6 +73,6 @@ fixperms:
 
 clean:
 	rm -f $($(IWAD)DEB) $($(IWAD)TARGETS)
-	rmdir $(DIRS)
+	-for dir in $(DIRS); do [ -d $$dir ] && rmdir $$dir; done
 
 PHONY: fixperms clean
