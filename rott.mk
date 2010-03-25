169cdb897e55c1ea3ae0b731a4ab006f4e957117
Jon Dowland <jon@kiriath.(none)>
2010-03-25 12:44:00 +0000

    rename various rott-data occurances to rott-data-shareware

--- rott.mk
+++ rott.mk
@@ -1,19 +1,19 @@
-BASICFILES = usr/share/doc/rott-data/README.Debian \
-usr/share/doc/rott-data/copyright
-DESTFILES = $(addprefix build/rott-data/, $(BASICFILES))
+BASICFILES = usr/share/doc/rott-data-shareware/README.Debian \
+usr/share/doc/rott-data-shareware/copyright
+DESTFILES = $(addprefix build/rott-data-shareware/, $(BASICFILES))
 
 # VERSION is defined by the parent make
-out/rott-data_$(VERSION)_all.deb: build/rott-data/DEBIAN/control $(DESTFILES)
-		fakeroot dpkg-deb -b build/rott-data $@
+out/rott-data-shareware_$(VERSION)_all.deb: build/rott-data-shareware/DEBIAN/control $(DESTFILES)
+	fakeroot dpkg-deb -b build/rott-data-shareware $@
 
-DIRS = build/rott-data \
-build/rott-data/DEBIAN \
-build/rott-data/usr \
-build/rott-data/usr/share \
-build/rott-data/usr/share/games \
-build/rott-data/usr/share/games/rott \
-build/rott-data/usr/share/doc \
-build/rott-data/usr/share/doc/rott-data
+DIRS = build/rott-data-shareware \
+build/rott-data-shareware/DEBIAN \
+build/rott-data-shareware/usr \
+build/rott-data-shareware/usr/share \
+build/rott-data-shareware/usr/share/games \
+build/rott-data-shareware/usr/share/games/rott \
+build/rott-data-shareware/usr/share/doc \
+build/rott-data-shareware/usr/share/doc/rott-data-shareware
 
 $(DIRS):
 	mkdir $@
@@ -21,13 +21,13 @@ $(DIRS):
 $(DESTFILES): $(DIRS)
 	cp -p rott-data/`basename "$@"` $@
 
-build/rott-data/DEBIAN/control: rott-data/control.in $(DIRS)
-	m4 -DPACKAGE=rott-data -DVERSION=$(VERSION) $< > $@ 
+build/rott-data-shareware/DEBIAN/control: rott-data/control.in $(DIRS)
+	m4 -DPACKAGE=rott-data-shareware -DVERSION=$(VERSION) $< > $@ 
 
 clean:
-	rm -f build/rott-data/DEBIAN/control out/rott-data_$(VERSION)_all.deb \
-		build/rott-data/usr/share/doc/rott-data/copyright \
-		build/rott-data/usr/share/doc/rott-data/README.Debian
+	rm -f build/rott-data-shareware/DEBIAN/control out/rott-data-shareware_$(VERSION)_all.deb \
+		build/rott-data-shareware/usr/share/doc/rott-data-shareware/copyright \
+		build/rott-data-shareware/usr/share/doc/rott-data-sharewara/README.Debian
 	for d in $(DIRS); do echo "$$d"; done | sort -r | while read d; do \
 		[ ! -d "$$d" ] || rmdir "$$d"; done
 
