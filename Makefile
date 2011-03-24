14f8e5cc36360fc40b254fe08c32fc89ea4c7eec
Jon Dowland <jmtd@debian.org>
2011-03-24 12:13:44 +0000

    initial wolf3d support

--- Makefile
+++ Makefile
@@ -12,6 +12,7 @@ default: $(DIRS) wolf/extract
 		LONG="Final Doom: The Plutonia Experiment" VERSION=$(VERSION)
 	make -f quake3.mk LONG="Quake III Arena" VERSION=$(VERSION)
 	make -f rott.mk VERSION=$(VERSION)
+	make -f wolf3d.mk VERSION=$(VERSION)
 
 wolf/extract: wolf/extract.c
 
@@ -28,6 +29,7 @@ clean:
 		LONG="Final Doom: The Plutonia Experiment" VERSION=$(VERSION) clean
 	make -f quake3.mk LONG="Quake III Arena" VERSION=$(VERSION) clean
 	make -f rott.mk VERSION=$(VERSION) clean
+	make -f wolf3d.mk VERSION=$(VERSION) clean
 	for d in $(DIRS); do [ ! -d "$$d" ]  || rmdir "$$d"; done
 	rm -f wolf/extract
 
---
+++ wolf3d.mk
@@ -0,0 +1,34 @@
+BASICFILES = usr/share/doc/wolf3d-data/README.Debian \
+usr/share/doc/wolf3d-data/copyright
+DESTFILES = $(addprefix build/wolf3d-data/, $(BASICFILES))
+
+# VERSION is defined by the parent make
+out/wolf3d-data_$(VERSION)_all.deb: build/wolf3d-data/DEBIAN/control $(DESTFILES)
+		fakeroot dpkg-deb -b build/wolf3d-data $@
+
+DIRS = build/wolf3d-data \
+build/wolf3d-data/DEBIAN \
+build/wolf3d-data/usr \
+build/wolf3d-data/usr/share \
+build/wolf3d-data/usr/share/games \
+build/wolf3d-data/usr/share/games/wolf3d \
+build/wolf3d-data/usr/share/doc \
+build/wolf3d-data/usr/share/doc/wolf3d-data
+
+$(DIRS):
+	mkdir $@
+
+$(DESTFILES): $(DIRS)
+	cp -p wolf3d-data/`basename "$@"` $@
+
+build/wolf3d-data/DEBIAN/control: wolf3d-data/control.in $(DIRS)
+	m4 -DPACKAGE=wolf3d-data -DVERSION=$(VERSION) $< > $@ 
+
+clean:
+	rm -f build/wolf3d-data/DEBIAN/control out/wolf3d-data_$(VERSION)_all.deb \
+		build/wolf3d-data/usr/share/doc/wolf3d-data/copyright \
+		build/wolf3d-data/usr/share/doc/wolf3d-data/README.Debian
+	for d in $(DIRS); do echo "$$d"; done | sort -r | while read d; do \
+		[ ! -d "$$d" ] || rmdir "$$d"; done
+
+.PHONY: clean $(DESTFILES)
